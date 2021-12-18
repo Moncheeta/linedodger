1a746ed5b85d2eb5cce3702d810f2b1ed35154d5
Moncheeta <monkey.damianek@gmail.com>
2021-12-18 14:29:42 -0600

    Improved background

--- game.py
+++ game.py
@@ -1,28 +1,24 @@
 import pygame
 import os
-import sys
 import random
 import schedule
-import itertools
-
-WIDTH, HEIGHT = 640, 320 #size of the window
 
 """
 Todo:
 """
 class linedodger():
     class Player():
-        def __init__(self, SCREEN):
+        def __init__(self, SCREEN, SCREEN_SIZE):
             self.size = 8
             self.SCREEN = SCREEN
-            self.WHITE = (255, 255, 255)
-            self.x = WIDTH/2
-            self.y = HEIGHT-20
+            self.WIDTH = SCREEN_SIZE[0]
+            self.HEIGHT = SCREEN_SIZE[1]
+            self.x = self.WIDTH/2
+            self.y = self.HEIGHT-20
             self.score = 0
-            self.vel = 2.5
+            self.vel = 2.8
             self.player = pygame.Rect(self.x, self.y, self.size, self.size)
-            pygame.draw.circle(self.SCREEN, self.WHITE, (self.x, self.y), self.size)
- 
+
         def update(self, lines):
             pressedkeys = pygame.key.get_pressed() #gets all pressed keys
             if pressedkeys[pygame.K_LEFT] and pressedkeys[pygame.K_RIGHT]:
@@ -33,16 +29,16 @@ class linedodger():
                 self.x += self.vel
             if self.x < self.size:
                 self.x = self.size
-            elif self.x > WIDTH - self.size:
-                self.x = WIDTH - self.size
-            pygame.draw.circle(self.SCREEN, self.WHITE, (self.x, self.y), self.size)
+            elif self.x > self.WIDTH - self.size:
+                self.x = self.WIDTH - self.size
+            pygame.draw.circle(self.SCREEN, (0, 255, 255), (self.x, self.y), self.size)
             self.player.topleft = self.x, self.y
             for line in lines:
                 for chunck in line.chuncks: #checks for collisons
                     if self.player.colliderect(chunck):
                         print("Contact: Game Over")
                         self.score = 0
-                        self.x = WIDTH/2
+                        self.x = self.WIDTH/2
                         return -1
                     else:
                         line.chuncks.remove(chunck)
@@ -50,14 +46,15 @@ class linedodger():
             return 0
 
     class Line():
-        def __init__(self, SCREEN):
+        def __init__(self, SCREEN, SCREEN_SIZE):
             self.y = 0
             self.SCREEN = SCREEN
-            self.WHITE = (255, 255, 255)
+            self.WIDTH = SCREEN_SIZE[0]
+            self.HEIGHT = SCREEN_SIZE[1]
             self.speed = 2
             self.width = 3
             self.block_size = 32
-            self.num_of_chuncks = round(WIDTH/self.block_size)
+            self.num_of_chuncks = round(self.WIDTH/self.block_size)
             number_of_holes = random.randint(1, 3)
             self.holepositions = []
             self.chuncks = []
@@ -70,7 +67,7 @@ class linedodger():
 
         def update(self):
             self.y += self.speed
-            if self.y >= HEIGHT:
+            if self.y >= self.HEIGHT:
                 return -1
             else:
                 for number in range(self.num_of_chuncks):
@@ -78,14 +75,43 @@ class linedodger():
                         pass
                     else:
                         x = (number * self.block_size)
-                        new_chunck = pygame.draw.rect(self.SCREEN, self.WHITE, (x, self.y, self.block_size, 3))
+                        new_chunck = pygame.draw.rect(self.SCREEN, (255, 0, 0), (x, self.y, self.block_size, 3))
                         new_chunck.topleft = x, self.y
                         self.chuncks.append(new_chunck)
                 return 0
 
+    class Background:
+        def __init__(self, WIDTH, HEIGHT):
+            self.objects = []
+            self.star = pygame.image.load(os.path.join("assets", "star.png")).convert_alpha()
+            self.galaxy = pygame.image.load(os.path.join("assets", "galaxy.png")).convert_alpha()
+            self.spawn_chance = 10
+            self.max_objects = 100
+
+        def sort_key(self, i):
+            return i["size"]
+
+        def update(self, SCREEN, WIDTH, HEIGHT, speed):
+            SCREEN.fill((32, 25, 26))
+            SCREEN.blit(self.galaxy, ((WIDTH-self.galaxy.get_width())/2, (HEIGHT-self.galaxy.get_height())/2))
+            if len(self.objects) != self.max_objects:
+                spawn = random.randint(0, self.spawn_chance)
+                if spawn == self.spawn_chance:
+                    new_object_size = random.randint(1, 10)
+                    new_object_x = random.randint(0, WIDTH)
+                    image = pygame.transform.scale(self.star, (new_object_size, new_object_size))
+                    self.objects.append({"object": image, "size": new_object_size, "x": new_object_x, "y": -50})
+            self.objects.sort(reverse=True, key=self.sort_key)
+            for flying_object in self.objects:
+                if flying_object["y"] > HEIGHT:
+                    self.objects.remove(flying_object)
+                else:
+                    SCREEN.blit(flying_object["object"], (flying_object["x"], flying_object["y"]))
+                    flying_object["y"] += speed/flying_object["size"]
+
     def updateWindow(self, player, lines, font):
         self.SCREEN.fill((0, 0, 0))
-        self.SCREEN.blit(self.BACKGROUND, (0, 0)) #puts the BACKGROUND
+        self.bg.update(self.SCREEN, self.WIDTH, self.HEIGHT, 10) #puts the BACKGROUND
         update_player = player.update(lines)
         if update_player != 0:
             return update_player
@@ -95,50 +121,49 @@ class linedodger():
                 self.lines.remove(line)
                 del line
         display_score = self.font.render(str(player.score), False, (255, 255, 255))
-        self.SCREEN.blit(display_score, (WIDTH - 64*len(str(player.score)), 20))
+        self.SCREEN.blit(display_score, (self.WIDTH - 64*len(str(player.score)), 20))
         pygame.display.update() #updates/refreshes the SCREEN
 
     def new_line(self):
-        new_line = self.Line(self.SCREEN)
+        new_line = self.Line(self.SCREEN, (self.WIDTH, self.HEIGHT))
         self.lines.append(new_line)
 
     def game_over(self, score): #:(
-        for line in self.lines: 
+        for line in self.lines:
             self.lines.remove(line)
             del line
 
     def run(self):
         create_new_lines = schedule.every(int(self.MIN)).seconds.to(int(self.MAX)).do(self.new_line)
-        self.player.x = WIDTH/2
+        self.player.x = self.WIDTH/2
         while True:
             self.clock.tick(self.FPS) #makes sure that the game stays at 60 fps
             schedule.run_pending()
             if_error = self.updateWindow(self.player, self.lines, self.font) #updates the window with the new player position
             if if_error == -1:
-                return self.player.score
+                return -1
             for event in pygame.event.get(): #checks if you close the window and then stops the game
                 if event.type == pygame.QUIT:
-                    sys.exit()
+                    return -2
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_q:
-                        sys.exit()
+                        return -2
                     elif event.key == pygame.K_r:
-                        return self.player.score
+                        return -1
         del self.player
 
-    def main_loop(self):
-        self.SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+    def main_loop(self, WIDTH, HEIGHT):
+        self.WIDTH = WIDTH
+        self.HEIGHT = HEIGHT
+        self.SCREEN = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         pygame.display.set_caption("Line Dodger")
-
         self.lines = []
         self.FPS = 60 #frames per second
-
-        self.BACKGROUND = pygame.image.load(os.path.join("assets", "stars.jpg")).convert() #BACKGROUND
-
+        self.bg = self.Background(self.WIDTH, self.HEIGHT) #BACKGROUND
         self.MIN = 3 #min amount of time to spawn a line
         self.MAX = 5 #max amount of time to spawn a line
         self.clock = pygame.time.Clock()
-        self.player = self.Player(self.SCREEN)
+        self.player = self.Player(self.SCREEN, (self.WIDTH, self.HEIGHT))
         pygame.init()
         pygame.font.init()
         pygame.mixer.init() #starts the player
@@ -147,11 +172,14 @@ class linedodger():
         self.font = pygame.font.SysFont("Arial", 64)
         while True:
             run_game = self.run()
-            if run_game != -1:
+            if run_game == -1:
                 self.game_over(run_game)
+            elif run_game == -2:
+                break
             schedule.clear()
             self.player.score = 0
 
 if __name__ == "__main__":
+    WIDTH, HEIGHT = 640, 320 #size of the window
     game = linedodger()
-    game.main_loop()
+    game.main_loop(WIDTH, HEIGHT)
